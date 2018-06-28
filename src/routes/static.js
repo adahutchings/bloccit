40aa8c5319175ef58c705767a5ee510f067dc516
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-06-28 10:03:09 -0700

    Assignment 1 : Complete

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.send("Welcom to Bloccit");
+    res.send("Welcome to Bloccit");
+});
+
+router.get("/macro", (req, res, next) => {
+    res.send("polo");
 });
 
 module.exports = router;
