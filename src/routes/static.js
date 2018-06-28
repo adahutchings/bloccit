df748779f923df41ace31a75e51e924657d7121f
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-06-28 13:59:10 -0700

    Checkpoint 2 : Complete

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.send("Welcom to Bloccit");
+    res.send("Welcome to Bloccit");
 });
 
 module.exports = router;
