56484d0c0ad46658b77643ebcbd01e0dff87ec44
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-07-05 20:24:57 -0700

    Checkpoint 6 version 2 : Complete

---
+++ src/db/seeds/20180706031841-topics.js
@@ -0,0 +1,39 @@
+'use strict';
+const facker = require("faker");
+let topics = [];
+
+for(let i = 1; i <= 15; i++){
+  topics.push({
+    title: facker.hacker.noun(),
+    description: facker.hacker.phrase(),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  });
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert("Topics", topics, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+   return queryInterface.bulkDelete("Topics", null, {});
+  }
+};
