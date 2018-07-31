a6328bc92269a9313c15c693633bb77660b4dfb5
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-07-30 21:31:35 -0700

    Unit Tests Complete

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -33,6 +33,12 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "postId",
       as: "comments"
     });
+
+    Post.hasMany(models.Vote, {
+      foreignKey: "postId",
+      as: "votes"
+    });
+ 
   };
   return Post;
 };
